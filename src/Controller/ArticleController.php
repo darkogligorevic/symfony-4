d0dbc5d484b6dc8e5adb574c56c458a73e97e2d6
Darko Gligorevic <darkogligorevic90@gmail.com>
2019-06-10 13:48:30 +0200

    3

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,12 +4,26 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('Radi li ovo?');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf('vraca prvi clanak: %s',
+            $slug
+        ));
     }
 }
